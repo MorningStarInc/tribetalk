2af8f1c036a568a1ddfdedf0ce0c5573a8829b8b
MorningStarInc <117900152+MorningStarInc@users.noreply.github.com>
2023-06-29 12:13:04 +0100

    Add files via upload

---
+++ IM/nw.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        new_user = User(email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
